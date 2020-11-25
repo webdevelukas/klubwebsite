b777538db9ea2b97f7603e118cc4e0df8a48df76
Lukas Kreidenweis <lukas@deinklub.de>
2020-11-25 21:03:43 +0100

    add src folder

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import ResponsiveImage from "elements/ResponsiveImage";
+import ResponsiveImage from "../elements/ResponsiveImage";
 
 type PartnerProps = {
   logoUrl: string;
