ed80db2ece0ac1281d38c455b031326a1b5d55a1
Lukas Kreidenweis <lukas@deinklub.de>
2020-12-04 11:55:19 +0100

    :sparkles: add placeholder for buttons and member login

--- src/components/layout/Header/DesktopHeader.tsx
+++ src/components/layout/Header/DesktopHeader.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import NextLink from "next/link";
 import { colors } from "styles/colors";
 import { useState } from "react";
+import BecomeAMember from "components/BecomeAMember";
 
 type DesktopHeaderProps = {
   headerIsVisible: boolean;
@@ -12,45 +13,84 @@ export function DesktopHeader({ headerIsVisible }: DesktopHeaderProps) {
 
   return (
     <Header headerIsVisible={headerIsVisible}>
-      <NextLink href="/" passHref>
-        <a>
-          <Logo src="/tsv-paunzhausen.png" alt="club logo" />
-        </a>
-      </NextLink>
-      <H2>TSV Paunzhausen</H2>
-
-      <Nav onMouseLeave={() => setShowSubmenu(false)}>
-        <List>
-          <li onMouseEnter={() => setShowSubmenu(true)}>
-            Klub
-            {showSubmenu && (
-              <SubMenu
-                showSubmenu={showSubmenu}
-                onMouseLeave={() => setShowSubmenu(false)}
-              >
-                <List>
-                  <li>Klub</li>
-                  <li>Abteilungen</li>
-                  <li>Termine</li>
-                  <li>Shop</li>
-                  <li>Jobs</li>
-                  <li>Partner</li>
-                  <li>Starkbierfest</li>
-                </List>
-              </SubMenu>
-            )}
-          </li>
-          <li>Abteilungen</li>
-          <li>Termine</li>
-          <li>Shop</li>
-          <li>Jobs</li>
-          <li>Partner</li>
-          <li>Starkbierfest</li>
-        </List>
-      </Nav>
+      <GridContainer>
+        <NextLink href="/" passHref>
+          <a>
+            <Logo src="/tsv-paunzhausen.png" alt="club logo" />
+          </a>
+        </NextLink>
+        <H2>TSV Paunzhausen</H2>
+        <Nav onMouseLeave={() => setShowSubmenu(false)}>
+          <List>
+            <li onMouseEnter={() => setShowSubmenu(true)}>
+              Klub
+              {showSubmenu && (
+                <SubMenu
+                  showSubmenu={showSubmenu}
+                  onMouseLeave={() => setShowSubmenu(false)}
+                >
+                  <List>
+                    <li>Klub</li>
+                    <li>Abteilungen</li>
+                    <li>Termine</li>
+                    <li>Shop</li>
+                    <li>Jobs</li>
+                    <li>Partner</li>
+                  </List>
+                </SubMenu>
+              )}
+            </li>
+            <li>Abteilungen</li>
+            <li>Termine</li>
+            <li>Shop</li>
+            <li>Jobs</li>
+            <li>Partner</li>
+          </List>
+        </Nav>
+        <Wrapper>
+          <p>
+            Mitglieder Login <img src="/member-login.png" />
+          </p>
+          <BecomeAMember />
+        </Wrapper>
+      </GridContainer>
     </Header>
   );
 }
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto 1fr auto;
+  align-items: center;
+  grid-column-gap: 1rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+  height: 100%;
+  align-items: center;
+
+  button {
+    padding: 1rem;
+    font-size: 0.75rem;
+  }
+
+  p {
+    color: white;
+    text-align: right;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  img {
+    margin-left: 0.5rem;
+  }
+`;
 
 const Nav = styled.nav`
   height: 100%;
@@ -85,10 +125,6 @@ const List = styled.ul`
 
 const Header = styled.header<{ headerIsVisible: boolean }>`
   position: fixed;
-  display: grid;
-  grid-template-columns: auto auto 1fr;
-  align-items: center;
-  grid-column-gap: 1rem;
   width: 100%;
   height: 6.5rem;
   padding: 0 0.75rem;
@@ -107,7 +143,7 @@ const Header = styled.header<{ headerIsVisible: boolean }>`
 const Logo = styled.img`
   display: block;
   width: auto;
-  height: 4rem;
+  height: 5rem;
 `;
 
 const H2 = styled.h2`
