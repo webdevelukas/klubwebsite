dc36b46847e974f20da8b117ea9bb8f93b0da9b7
Lukas Kreidenweis <lukas@deinklub.de>
2021-07-05 09:23:50 +0200

    add sample data

---
+++ src/api/matches.js
@@ -0,0 +1,104 @@
+export const matches = [
+  {
+    homeTeam: {
+      name: "",
+      logo: {
+        url: "",
+        alt: "",
+      },
+    },
+    awayTeam: {
+      name: "",
+      logo: {
+        url: "",
+        alt: "",
+      },
+    },
+    result: "",
+    date: "21.06.2021",
+    time: "12:00",
+  },
+  {
+    homeTeam: {
+      name: "",
+      logo: {
+        url: "",
+        alt: "",
+      },
+    },
+    awayTeam: {
+      name: "",
+      logo: {
+        url: "",
+        alt: "",
+      },
+    },
+    result: "",
+    date: "17.06.2021",
+    time: "08:00",
+    competitionName: "Freundschaftsspiel",
+  },
+  {
+    homeTeam: {
+      name: "TSV Paunzhausen",
+      logo: {
+        url: "https://www.tsvpaunzhausen.de/themes/tsv_paunzhausen/images/logo.png",
+        alt: "",
+      },
+    },
+    awayTeam: {
+      name: "ST Scheyern",
+      logo: {
+        url: "",
+        alt: "",
+      },
+    },
+    result: "2:1",
+    date: "23.06.2021",
+    time: "14:00",
+    competitionName: "413 Kreisklasse 3",
+    matchday: 23,
+    newsUrl: "/",
+  },
+  {
+    homeTeam: {
+      name: "TSV Paunzhausen",
+      logo: {
+        url: "https://www.tsvpaunzhausen.de/themes/tsv_paunzhausen/images/logo.png",
+        alt: "",
+      },
+    },
+    awayTeam: {
+      name: "ST Scheyern",
+      logo: {
+        url: "",
+        alt: "",
+      },
+    },
+    result: "2:1",
+    date: "24.06.2021",
+    time: "14:00",
+    competitionName: "413 Kreisklasse 3",
+    matchday: 23,
+    newsUrl: "",
+  },
+  {
+    homeTeam: {
+      name: "",
+      logo: {
+        url: "https://www.tsvpaunzhausen.de/themes/tsv_paunzhausen/images/logo.png",
+        alt: "",
+      },
+    },
+    awayTeam: {
+      name: "",
+      logo: {
+        url: "",
+        alt: "",
+      },
+    },
+    result: "2:1",
+    date: "23.06.2021",
+    time: "08:25",
+  },
+];
---
+++ src/api/teamMembers.js
@@ -0,0 +1,37 @@
+export const teamMembers = [
+  {
+    image: {
+      url: "https://images.unsplash.com/photo-1597179300169-dce9b5b530f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      alt: "",
+    },
+    name: "Jerome Pascal",
+  },
+  {
+    image: {
+      url: "https://images.unsplash.com/photo-1598518619776-eae3f8a34eac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+      alt: "",
+    },
+    name: "Jordan Hendrakson",
+  },
+  {
+    image: {
+      url: "https://images.unsplash.com/photo-1500592624160-cadc1677c0b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      alt: "",
+    },
+    name: "Selin Pazkal",
+  },
+  {
+    image: {
+      url: "https://images.unsplash.com/photo-1617975550828-26af98396018?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      alt: "",
+    },
+    name: "Raymund Cerveza",
+  },
+  {
+    image: {
+      url: "",
+      alt: "",
+    },
+    name: "Frida Fischer",
+  },
+];
