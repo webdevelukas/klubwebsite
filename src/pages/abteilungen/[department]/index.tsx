0f11c569b17b820d5bb94b902ee174070a626a81
Lukas Kreidenweis <lukas@deinklub.de>
2021-07-07 13:31:34 +0200

    fix bug with mockup data

--- src/pages/abteilungen/[department]/index.tsx
+++ src/pages/abteilungen/[department]/index.tsx
@@ -146,7 +146,7 @@ type DepartmentProps = {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const departments = mainNavItems[1].submenuItems;
+  const departments = mainNavItems[2].submenuItems;
 
   const paths = departments.map((department: DepartmentProps) => ({
     params: { department: department.slug },
